f7bdf29a67e15a50b636a2c278d5b788ed98f3e6
nguyenquyduong123123-ctrl <nguyenquyduong123123@gmail.com>
2026-04-22 16:26:25 +0700

    Revert "dm dang beo"
    This reverts commit 6137464cb5132ce9297835a32b58b4074b3d5cb1.

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from PyQt5 import QtWidgets
 from login_window import CuaSoDangNhap
 
 if __name__ == "__main__":
+    # Trỏ về thư mục gốc chứa TẤT CẢ code, ui, qss (không trỏ vào thư mục picture)
 
-    sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-    
     app = QtWidgets.QApplication(sys.argv)
 
     main_window = CuaSoDangNhap()
     main_window.show()
+
+
+
 
     sys.exit(app.exec())
